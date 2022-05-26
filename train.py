a0ef63f039aa8a977b3158dbf5a848151069bfca
SUN3107 <81905196+SUN3107@users.noreply.github.com>
2022-05-26 23:41:17 +0530

    Add face recognition
    LBPH algorithm

--- train.py
+++ train.py
@@ -1,31 +1,43 @@
-from cProfile import label
-from ctypes import alignment
-from json.tool import main
 from re import L
 from tkinter import*
-from tkinter import ttk
-from tkinter import scrolledtext
-from PIL import Image,ImageTk
-from colorama import Style
+from tkinter import messagebox
+from PIL import Image
+import cv2
+import os
+import numpy as np
 
 class train_data:
-    def __init__(self,root):
-        self.root=root
-        self.root.geometry("1920x1080")
-        self.root.title("Train Data")
-
-
-        #title
-        my_label = Label(text="Train Data", font=("times new roman",36,"bold", "italic"), bg='white', fg="red")
-        my_label.place(x=473,y=83)
-
-        train_button=Button(self.root,highlightthickness = 0, bd = 0,text="Train Data",font=("Quicksand",16,"bold"),bg='white', fg="darkblue",compound="top",cursor="hand2")
-        train_button.place(x=845,y=623,width=238,height=281)
-
-
 
     def train_classify(self):
-        
+        data_dir=("face_data")
+        path = [ os.path.join(data_dir,file) for file in os.listdir(data_dir)]
+
+        faces=[]
+        ids=[]
+
+        for image in path:
+            img=Image.open(image).convert('L')
+            img_np=np.array(img,'uint8')
+            id=int(os.path.split(image)[1].split('.')[1])
+
+            faces.append(img_np)
+            ids.append(id)
+            cv2.imshow("Training",img_np)
+            cv2.waitKey(1)==13
+
+        ids=np.array(ids)
+
+
+        #Train the classifier
+
+        classf=cv2.face.LBPHFaceRecognizer_create()
+        classf.train(faces,ids)
+        classf.write("Classifier_data.xml")
+        cv2.destroyAllWindows()
+
+        messagebox.showinfo("Success","Training dataset completed")
+
+
 
 
 
